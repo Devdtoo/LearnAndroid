d54a3c58a85b8b1c7ef7e0272802178092ae0234
Dev Chaturvedi <dev@Devs-MacBook-Air.local>
2022-04-25 19:43:53 +0530

    Initial commit

---
+++ app/src/main/java/com/devdtoo/learnandroid/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.devdtoo.learnandroid
+
+import android.app.Notification
+import android.app.NotificationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    
+}
